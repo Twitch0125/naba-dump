757288bdbbc24bddd6e9d10109c87513c68de7ac
Kaleb Ercanbrack <kaleb.ercanbrack@hey.com>
2023-03-26 02:07:12 -0600

    fix script

--- deploy.js
+++ deploy.js
@@ -2,9 +2,11 @@ import tar from 'tar'
 import fs from 'node:fs/promises'
 console.log("clearing data dir")
 await fs.rm('data', {recursive: true, force: true})
+await fs.mkdir('data')
 console.log('extracting site');
 await tar.extract({
     file: 'reports.tar.gz',
     C:'data'
 })
 console.log('extracted to data/');
+console.log('done!');
